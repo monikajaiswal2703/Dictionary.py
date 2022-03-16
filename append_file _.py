9d59016b1ccdf76e067142edf8242bd135cb73f1
Monika Jaiswal <92169645+monikajaiswal2703@users.noreply.github.com>
2022-03-16 10:41:13 +0530

    Add files via upload

---
+++ append_file _.py
@@ -0,0 +1,29 @@
+# f = open("demo.txt", "a")
+# f.write("Now the file has more content!")
+# f.close()
+
+# f=open("priynka.txt","r")
+# b=f.read()
+# print(b)
+# f.close()
+
+# f = open("demofile2.txt", "r")
+# print(f.read()) 
+# f = open("myfile.txt", "x")
+# f.closed()
+
+# import os
+# os.remove("demo.txt") 
+# import os
+# if os.path.exists("demofile.txt"):
+#   os.remove("demofile.txt")
+# else:
+#   print("The file does not exist") 
+
+# import os
+# os.rmdir("veriable")
+
+a=open("alive_gamefile.txt","r")
+b=a.read()
+print(b)
+ 
