3b4b9892f5e0a468c9e5ad6f2e88139ccae75999
Monika Jaiswal <92169645+monikajaiswal2703@users.noreply.github.com>
2022-03-16 14:04:36 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,21 @@
+# a="navvgurrukul"
+# b=a
+# d={}
+# for i in b:
+#     if i in d:
+#         d[i]+=1
+#     else:
+#         d[i]=1
+# print(d)
+# def subtractProductAndSum(n):
+#         m=1
+#         s=0
+#         while n>0:
+#             m=m*(n%10)
+#             # n//=10
+#         # return m
+#             s=s+(n%10)
+#             n//=10
+#         return (m-s)
+# print(subtractProductAndSum(234))
+
